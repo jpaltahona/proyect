3a042c4ca28839812f025a3b39e565adeb1df791
jean <jpaltahonacc@gmail.com>
2019-02-01 12:37:22 -0500

    incorporando boostrarp

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { Button } from 'react-bootstrap';
 
 import './App.css';
 
 class App extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            names: []
-        }
-        axios.get('http://localhost:3003/name')
-            .then(response => {
-               this.setState({
-                names: response.data
-               })
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    }
-
   render() {
 
     return (
       <div>
-        <h1>hola mundo</h1>
-
-        <ul>
-            {this.state.names.map(name =>
-                <li key={name}>{name}</li>    
-            )}
-        </ul>
+          <h1>hola mundo</h1>
+            <Button bsStyle="primary"> primario</Button>
       </div>
     );
   }
