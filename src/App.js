f657392377d7418caf2f168b42d669c5f2871011
jean <jpaltahonacc@gmail.com>
2019-02-01 09:52:56 -0500

    recogiendo informacion de formularios

--- src/App.js
+++ src/App.js
@@ -5,41 +5,46 @@ import './App.css';
 
 class App extends Component {
 
-    constructor() {
+    constructor(){
         super();
-    
         this.state = {
-            task: ['tarea 1', 'tarea 2', 'tarea 3']
+            name: 'juan',
+            terms: false
         }
     }
-    
-    addTask(){
+
+    updateName(event){
         this.setState({
-            task: this.state.task.concat('nuevo elemento')
-        })
+            name: event.target.value
+        });
+    }
+    updateTerms(event){
+        this.setState({
+            terms: event.target.checked
+        });
     }
 
-    updateTask(){
-        const index = this.state.task.findIndex(task => task === "tarea 1");
-
-        this.setState({
-            task: this.state.task.map((task, i) =>
-                i === index ? 'actaulizado' : task
-            )
-        })
+    sayHI(){
+     if(this.state.terms){
+        alert('hola ' + this.state.name)
+     }   else{
+         alert('deves aceptar los terminos')
+     }
+        
     }
 
   render() {
 
     return (
       <div>
-        <h3>Hola mundo</h3>
-        <ul>
-            {this.state.task.map((task, index) =>
-                <li key={index}>{task}</li>)}
-        </ul>
-        <button onClick={this.addTask.bind(this)}>Agregar nuevo</button>
-        <button onClick={this.updateTask.bind(this)}>Modificar</button>
+        <h2>Formularios</h2>
+        <input type="text"  placeholder={this.state.name} onChange={this.updateName.bind(this)}/>
+        <div>
+            <label>
+                <input type="checkbox"checked={this.state.terms} onChange={this.updateTerms.bind(this)}/> acepto los terminos y condiciones
+            </label>
+        </div>
+        <button onClick={this.sayHI.bind(this)}>Say HI!</button>
       </div>
     );
   }
